42095e0c960184eb71e2a795d5876bd83335bc86
error411 <error411@gmail.com>
2026-03-31 13:40:24 -0700

    date fixes

--- src/app/follow-ups/page.tsx
+++ src/app/follow-ups/page.tsx
@@ -6,7 +6,7 @@ import {
 } from '@/lib/applications/get-follow-up-state'
 import type { Tables } from '@/lib/supabase/types'
 import FollowUpActions from './follow-up-actions'
-import { formatDate } from '@/lib/dates'
+import { formatDate, formatDateTime } from '@/lib/dates'
 
 type ApplicationRow = Tables<'applications'>
 type JobRow = Tables<'jobs'>
