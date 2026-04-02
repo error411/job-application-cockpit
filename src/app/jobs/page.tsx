6aa913c0889ca6a1e22495c2e7cbb54d38e6e277
error411 <error411@gmail.com>
2026-04-01 18:44:16 -0700

    another link fix for scoring on jobs page

--- src/app/jobs/page.tsx
+++ src/app/jobs/page.tsx
@@ -428,7 +428,7 @@ export default async function JobsPage() {
                       </Link>
 
                       <Link
-                        href={`/jobs/${job.id}/score`}
+                        href={`/jobs/${job.id}`}
                         className="app-button inline-flex items-center justify-center rounded-xl px-4 py-2 text-sm font-medium"
                       >
                         Score
